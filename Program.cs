45745a3888c0d1e72a5e5a0dfdef3345a012a5e5
jmojzes21 <jmojzes21@student.foi.hr>
2023-05-03 12:49:08 +0200

    Rename Form1 to MainForm #1

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace TextFileToUi
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
